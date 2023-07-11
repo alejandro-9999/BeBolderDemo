2bbecc3ec32100dbed9076d666998ddea0479295
= <=>
2023-07-11 12:36:07 -0500

    added files

---
+++ vacation-requests-service/src/main/java/com/bebolder/vacationrequestsservice/VacationRequestsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bebolder.vacationrequestsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRequestsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationRequestsServiceApplication.class, args);
+	}
+
+}
---
+++ vacation-requests-service/src/test/java/com/bebolder/vacationrequestsservice/VacationRequestsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebolder.vacationrequestsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationRequestsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
